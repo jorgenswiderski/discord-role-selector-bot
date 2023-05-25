2d87d2251fb35e10ea8aaa816107551d7686baf3
Jorgen Swiderski <jorgen.swiderski@gmail.com>
2023-05-25 12:45:58 -0500

    Fix member str function

--- util.py
+++ util.py
@@ -6,4 +6,4 @@ def copy(obj):
     return json.loads(json.dumps(obj))
 
 def get_member_str(member: hikari.Member):
-    return f"{member.nickname}' ({member.username}#{member.discriminator})"
+    return f"'{member.nickname if member.nickname is not None else member.username}' ({member.username}#{member.discriminator})"
