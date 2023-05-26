8e271346c5c36814ad54714d59c694594f66ef8b
Jorgen Swiderski <jorgen.swiderski@gmail.com>
2023-05-26 16:56:00 -0500

    Add prometheus for monitoring

---
+++ prometheus/start_prometheus.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+config_dir="/prometheus"
+envsubst < /prometheus/alertmanager.template.yml > alertmanager.yml
+
+# Stop Prometheus
+prometheus_pid=$(pgrep -f prometheus)
+if [[ -n $prometheus_pid ]]; then
+  echo "Stopping Prometheus (PID: $prometheus_pid)..."
+  kill $prometheus_pid
+  sleep 5
+fi
+
+# Stop Alertmanager
+alertmanager_pid=$(pgrep -f alertmanager)
+if [[ -n $alertmanager_pid ]]; then
+  echo "Stopping Alertmanager (PID: $alertmanager_pid)..."
+  kill $alertmanager_pid
+  sleep 5
+fi
+
+cd $config_dir
+
+# Start Prometheus
+echo "Starting Prometheus..."
+./home/ec2-user/prometheus-2.32.0.linux-amd64/prometheus
+
+# Start Alertmanager
+echo "Starting Alertmanager..."
+./home/ec2-user/alertmanager-0.25.0.linux-amd64/alertmanager
+
+echo "Prometheus and Alertmanager restarted successfully."
